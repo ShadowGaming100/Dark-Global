af07571909430dc1daf8b95e82d35abe116eb7d5
Shadow Gaming <81495140+ShadowGaming100@users.noreply.github.com>
2022-11-17 09:33:43 +0000

    Changes Added client module Installed package

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+//Import the Module
+const { MessageEmbed, MessageActionRow, MessageButton, MessageSelectmenu, MessageAttachment, Interaction, Discord, InteractionCreate, Client, Collection } = require('discord.js');
 const { QuickDB } = require("quick.db");
- const db = new QuickDB({ filePath: "Database/settings.sqlite"});
-
+const clientSettingsObject = require(`${process.cwd()}/Structures/Functions/clientSettingsObject.js`);
+const client = new Client(clientSettingsObject());
+require('dotenv').config();
+require('colors');
+require('ms');
 
 // loading JSON's files
 const connections = require('./Structures/Settings/connections.json')
@@ -8,8 +13,12 @@ const settings = require('./Structures/Settings/settings.json')
 const channels = require('./Structures/Settings/channels.json')
 const emoji = require('./Structures/Settings/emoji.json')
 
+// creating/loading database
+ const db = new QuickDB({ filePath: "Database/settings.sqlite"});
+
 //check if in connections hosting is set to true
     if( `${connections.hosting}` || process.env.hosting === "true"){
+
 //if set to true it create a server 
 const express = require('express');
 const app = express();
@@ -22,3 +31,5 @@ app.listen(3000, () => {
   console.log('server started');
 });
     }
+
+//login to bot
