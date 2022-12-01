cb1b52e8e0f9140a0e42f862ba66239e81129f80
Shadow Gaming <81495140+ShadowGaming100@users.noreply.github.com>
2022-12-01 06:03:33 +0000

    Updated security issues

--- test.js
+++ test.js
@@ -45,11 +45,19 @@ client.on("ready", () => {
 // CREATING SERVER IF SET TO TRUE IN connections.json IN hosting
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const { rateLimit } = require('express-rate-limit')
 const port = process.env.PORT || connections.port || 80;
 
 // sendFile will go here
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+app.use(limiter)
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/Website/index.html'));
 });
